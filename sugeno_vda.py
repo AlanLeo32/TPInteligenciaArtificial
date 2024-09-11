d5d321ec6b840e6033461355bc3c296289af85e5
AlanLeo32 <leonel1234alan@gmail.com>
2024-09-11 11:50:00 -0300

    Clustering Substractivo
    Faltaria hacer un buen testeo

--- sugeno_vda.py
+++ sugeno_vda.py
@@ -1,34 +1,97 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 import math
+from sklearn.preprocessing import MinMaxScaler
+
+def ClusteringSubstractivo(puntos,centros_clusters,N):
+    vec_pot = np.zeros(N)
+    max_pot = 0
+    c=0
+    #hiperparametros
+    Ra=0.6
+    Rb=0.8
+    RR=0.5 #coeficiente de rechazo. Tiene que ser menor que AR? 
+    AR=0.7 #coeficiente de aceptacion
+    k=0
+    for i in range (N):
+        for j in range(N):
+            if(i!=j):
+                dist=calcular_distancia(puntos[i],puntos[j])
+                vec_pot[i]+=np.exp(-dist**2/(Ra/2)**2)
+    k=np.argmax(vec_pot) # posicion del punto de maximo potencial
+    centros_clusters.append(puntos[k]) 
+    while(np.max(vec_pot) >= 1e-2):
+        Pk=np.max(vec_pot)
+        recalcular_potencial(puntos,vec_pot,k,Rb)
+        max_pot=np.max(vec_pot)
+        k=np.argmax(vec_pot)
+        if(max_pot>AR*Pk):
+            centros_clusters.append(puntos[k])
+        else:
+            if(max_pot<RR*Pk):
+                break
+            else:
+                Dr=distancia_minima(puntos[k],centros_clusters)
+                if((Dr/Ra)+(Pk/max_pot)>=1):
+                    centros_clusters.append(puntos[k])
+                else:
+                    vec_pot[k]=0
+        
+def calcular_distancia(p1, p2):
+    return math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
+
+def recalcular_potencial(puntos, vec_pot,c, Rb):
+    for i in range(len(puntos)):
+        dist = calcular_distancia(puntos[i],puntos[c])
+        vec_pot[i] -= vec_pot[i] * vec_pot[c]*np.exp(-(dist**2) / (Rb/2)**2)
+        if vec_pot[i]<0:
+            vec_pot[i]=0
+
+def distancia_minima(punto,puntos):
+# Encontrar la distancia mínima
+    min= float('inf')  # Inicializamos con infinito
+
+    for p in puntos:
+        distancia = calcular_distancia(punto, p)
+        if distancia < min:
+            min = distancia
+    return min
+
+#main
+
 Y=np.loadtxt("samplesVDA1.txt")
 X=np.arange(0,len(Y)*1/400,1/400)
 puntos = list(zip(X, Y))
-N = len(puntos) 
 
+centros_clusters=[]
 plt.plot(X, Y, marker='o')  # marker='o' es para mostrar los puntos, puedes omitirlo si no quieres puntos
-plt.xlabel('Eje X')  # Etiqueta para el eje X
-plt.ylabel('Eje Y')  # Etiqueta para el eje Y
-plt.title('Gráfico de X vs Y')  # Título del gráfico
+plt.xlabel('Tiempo')  # Etiqueta para el eje X
+plt.ylabel('Señal')  # Etiqueta para el eje Y
+plt.title('Gráfico de Tiempo vs Señal')  # Título del gráfico
 plt.grid(True)  # Mostrar una cuadrícula
 plt.show()  # Mostrar el gráfico
 
+#NORMALIZACION DE DATOS
+data = np.array(puntos)
+# Crea una instancia del escalador MinMaxScaler
+scaler = MinMaxScaler()
 
-#def ClusteringSubstractivo(puntos):
-    vec_pot = np.zeros(N)
-    max_pot = 0
+# Ajusta (fit) el escalador a tus datos para aprender los parámetros de la transformación
+scaler.fit(data)
 
-    #hiperparametros
-   # ar=
-    #rr= 
-    for i in range (N):
-        vec_pot(i) = CalculoDePotencial(N,puntos,Ra,i)
-        if (max_pot < vec_pot(i))
-           max_pot = vec_pot(i)
+# Luego, puedes usar el método transform para normalizar tus datos
+normalized_data = scaler.transform(data)
+puntos_normalizados = tuple(map(tuple, normalized_data))
+X,Y=zip(*puntos_normalizados)
+puntos = list(zip(X, Y))
+plt.plot(X, Y, marker='o')  # marker='o' es para mostrar los puntos, puedes omitirlo si no quieres puntos
+plt.xlabel('Tiempo')  # Etiqueta para el eje X
+plt.ylabel('Señal')  # Etiqueta para el eje Y
+plt.title('Gráfico de Tiempo vs Señal Normalizado')  # Título del gráfico
+plt.grid(True)  # Mostrar una cuadrícula
+plt.show()  # Mostrar el gráfico
 
-    
-def CalculoDePotencial(N,puntos,R,i):
-    sum = 0
-    for j in range(N):
-       sum += math.e**(-(np.linalg.norm(puntos(i) - puntos(j)))**2 /((R/2)**2))
-    return 
+N = len(puntos_normalizados) 
+centros_clusters=[]
+ClusteringSubstractivo(puntos_normalizados,centros_clusters,N)
+print(np.float64(centros_clusters))
